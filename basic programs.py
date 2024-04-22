df40f63e2388a2e1bf08bb5f7c7c470ec69f10a1
Arjun <144939625+Arjunmukundann@users.noreply.github.com>
2024-04-22 10:31:48 +0530

    basic program1.py
    Program for adding two numbers (using def function).

---
+++ basic programs.py
@@ -0,0 +1,10 @@
+Python program to add two numbers
+
+
+def sum(num1,num2):
+    result=num1+num2
+    print( "sum of",num1,"+ ",num2," =",result)
+    return result
+num1=int(input("enter first number"))
+num2=int(input("enter your second number"))
+sum(num1,num2)
