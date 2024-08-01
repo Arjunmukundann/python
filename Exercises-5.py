22559846592bc7f3408d85ccb697f91f628c3155
Arjun <144939625+Arjunmukundann@users.noreply.github.com>
2024-08-01 18:30:05 +0530

    Exercises-5.py

---
+++ Exercises-5.py
@@ -0,0 +1,13 @@
+Q:
+    To accept a numebr  and display the number along with its square,cube and square root.
+
+
+Here's the code
+    import math
+number=float(input("enter the number"))
+square=number*number
+cube=number*number*number
+squareroot=math.sqrt(number)
+print(f"square of {number} is {square}")
+print(f"cube of {number} is {cube}")
+print(f"square root of {number} is {squareroot}")
