43ac70d97166a9ac88b8e8e5b3333990faba8470
Arjun <144939625+Arjunmukundann@users.noreply.github.com>
2024-08-01 18:36:40 +0530

    Exercises-6.py

---
+++ Exercises-6.py
@@ -0,0 +1,7 @@
+Q;
+    To accept temperature in degree Celsius(c) and to dispaly it in dergree Fahrenheit usinf the formula 9/5*celsius+32
+
+Here's the code
+    celsius=float(input("enter the temperture in degree celsius"))
+fahrenheit=9/5*celsius+32
+print(f"{celsius} degree celsius is {fahrenheit} fahrenheit")
