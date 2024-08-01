52bbac9ac5407d848eb8b04ae259f08517efe328
Arjun <144939625+Arjunmukundann@users.noreply.github.com>
2024-08-01 17:51:12 +0530

    Exercises.py

---
+++ Exercises.py
@@ -0,0 +1,15 @@
+Q:To input integr quantities to the variable a,b,c and then perform the following (1) halves value of a(2) multiply by 2(3) square of the number and finally print result
+
+
+Here is the code
+
+
+
+
+print("enter the number")
+a=int(input("first number"))
+b=int(input("second number"))
+c=int(input("third numebr"))
+print("half of ",a,"is",a/2)
+print(b,"mulitiply by 2 is",b*2)
+print(c*c,"c multiply by itself")
