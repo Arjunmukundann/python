3f2720bee4580e9e05f57e52226a9c649ddadf1e
Arjun <144939625+Arjunmukundann@users.noreply.github.com>
2024-08-01 17:57:23 +0530

    Exercises-2.py

---
+++ Exercises-2.py
@@ -0,0 +1,11 @@
+Q: To accept the length,breadth,height  of a ractangular boxand to display its volume.
+ 
+
+here's the code
+
+print("enter the dimension of box")
+length=float(input("enter the length"))
+breadth=float(input("enter the breadth"))
+height=float(input("enter the height"))
+volume=length*breadth*height
+print("volume of the bos is ",volume,"cubic meter")
