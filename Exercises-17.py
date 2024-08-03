7f994639a0ee8796921b521fc2e32a6cd2974813
Arjun <144939625+Arjunmukundann@users.noreply.github.com>
2024-08-03 18:36:22 +0530

    Exercises-17.py

---
+++ Exercises-17.py
@@ -0,0 +1,13 @@
+Q:
+   To input 3 numbers and to  display the biggest among them.
+Here's the code:
+   n1=int(input("enter the first number"))
+n2=int(input("enter the second number"))
+n3=int(input("enter the 3rd number"))
+big=n1
+if(big<n2):
+    big=n2
+if big<n3:
+    big=n3;
+print(big,"is the biggest among ",n1,n2,n3)        
+    
