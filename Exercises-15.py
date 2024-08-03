c2b7b01d9c9e687d72e0eb040449f671e6cdab08
Arjun <144939625+Arjunmukundann@users.noreply.github.com>
2024-08-03 18:21:26 +0530

    Exercises-15.py

---
+++ Exercises-15.py
@@ -0,0 +1,9 @@
+Q;
+   To input a number and to display whether it is even or odd
+
+Here's the code:
+   number=int(input("enter the numebr"))
+if number%2==0:
+    print("%d is even"%number)
+else:
+    print(f"{number} is odd")    
