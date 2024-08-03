669c21a78729862b5e420bd2c29d98bc83941667
Arjun <144939625+Arjunmukundann@users.noreply.github.com>
2024-08-03 18:24:52 +0530

    Exercises-16.py

---
+++ Exercises-16.py
@@ -0,0 +1,8 @@
+Q:
+     To input a number and to display whether it is a multiple of 5 or not
+Here's the code:
+     number=int(input("enter the number"))
+if number%5==0:
+    print(f"{number} is mulitiple of 5")
+else:
+    print(f"{number} is not multiple of 5")    
